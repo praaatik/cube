2b75b95893464fe182f3ef7676d023cc336ba4cf
Pratik Kulkarni <pnkulkarni@proton.me>
2024-09-25 18:15:35 +0530

    update scheduler skeleton code

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -1,1 +1,9 @@
 package scheduler
+
+// Scheduler does something which I am not entirely sure of
+// TODO: Understand what the Scheduler *really* does and why is it an interface
+type Scheduler interface {
+	SelectCandidateNodes()
+	Score()
+	Pick()
+}
